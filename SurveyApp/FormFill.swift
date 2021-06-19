e1ee18ca323d61777a56300bb422d38349295e57
Ramesh Guddala <ramesh_guddala@Rameshs-MacBook-Air.local>
2021-06-19 17:56:46 +0530

    SurveyApp first

---
+++ SurveyApp/FormFill.swift
@@ -0,0 +1,20 @@
+//
+//  Questions.swift
+//  SurveyApp
+//
+//  Created by Ramesh Guddala on 14/06/21.
+//
+
+import Foundation
+
+// MARK: - Welcome
+struct Form: Codable {
+    let id, formStatus: String
+    let questions: [Question]
+}
+
+// MARK: - Question
+struct Question: Codable {
+    let answers: [String]
+    let question: String
+}
